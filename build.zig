02a2b54f940434a6a55a6f734c4e339a0ace12d9
LeRoyce Pearson <contact@leroycepearson.dev>
2023-08-16 23:00:30 -0600

    Fix `zig build test` not running test suite

--- build.zig
+++ build.zig
@@ -18,10 +18,11 @@ pub fn build(b: *std.build.Builder) void {
     });
     b.installArtifact(lib);
 
-    var main_tests = b.addTest(.{
+    const main_tests = b.addTest(.{
         .root_source_file = .{ .path = "jwt.zig" },
     });
+    const run_main_tests = b.addRunArtifact(main_tests);
 
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    test_step.dependOn(&run_main_tests.step);
 }
