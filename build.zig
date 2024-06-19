d722767da3ed5de6dcdb4f786958722a926733b3
yehuthi <yehuthith@gmail.com>
2024-06-19 09:27:14 +0300

    update build file

--- build.zig
+++ build.zig
@@ -1,25 +1,23 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const target = b.standardTargetOptions(.{});
     _ = b.addModule("jwt", .{
-        .source_file = .{ .path = "jwt.zig" },
+        .root_source_file = b.path("jwt.zig"),
     });
 
     const lib = b.addStaticLibrary(.{
         .name = "zig-jwt",
-        .root_source_file = .{ .path = "jwt.zig" },
+        .root_source_file = b.path("jwt.zig"),
         .optimize = optimize,
         .target = target,
     });
     b.installArtifact(lib);
 
     const main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "jwt.zig" },
+        .root_source_file = b.path("jwt.zig"),
     });
     const run_main_tests = b.addRunArtifact(main_tests);
 
